873030a39d41b785865ca7411b5ec2939f9605e9
pierre-antoine lambelin <palambelin@gmail.com>
2019-11-22 16:03:25 +0100

    tao changes

---
+++ p-projet.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-p-projet',
+  templateUrl: './p-projet.component.html',
+  styleUrls: ['./p-projet.component.scss']
+})
+export class PProjetComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
