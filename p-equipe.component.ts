df63de888f3c2ba1603d3f7aa422d70746b8cc6a
matau1405 <57523533+matau1405@users.noreply.github.com>
2019-11-24 14:10:14 +0100

    Add files via upload

---
+++ p-equipe.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-p-equipe',
+  templateUrl: './p-equipe.component.html',
+  styleUrls: ['./p-equipe.component.scss']
+})
+export class PEquipeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
